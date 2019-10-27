c6c622075b31bf60702cbdb2489d170c17fc9999
Jair ®odriguez <jair3337@hotmail.com>
2019-10-26 22:04:34 -0500

    integración con angularjs para vistas de usuario

---
+++ saerolinea/Scripts/Controllers/user.controller.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    init();
+})();
+
+function init() {
+    app.controller('userController', userController);
+    userController.$inject = ['$scope', '$http', 'userService', '$filter', 'repoService'];
+
+    function userController($scope, $http, userService, $filter) {
+
+        $scope.list = {};
+
+        $scope.List = () => {
+            userService.List()
+                .then(response => response.data)
+                .then(function (data) {
+                    if (data != null) {
+                        $scope.list = data.list;
+                    }
+                });
+        }
+        //Init
+        $scope.Init = () => {
+            $scope.List();
+        }
+
+        $scope.Init();
+    }
+}
---
+++ saerolinea/Scripts/Services/user.service.js
@@ -0,0 +1,5 @@
+﻿app.service('userService', function ($http, $q) {
+    this.BasicInfo = function () {
+        return $http.get('/User/Get')
+    }
+});
