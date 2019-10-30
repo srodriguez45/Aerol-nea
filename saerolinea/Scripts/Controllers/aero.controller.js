5a95fe77421154ca2d27777622eeb522adab536b
Jair ®odriguez <jair3337@hotmail.com>
2019-10-29 21:21:47 -0500

    actualización de controladores, vistas y servicios para vuelos

---
+++ saerolinea/Scripts/Controllers/aero.controller.js
@@ -0,0 +1,42 @@
+﻿/*
+ * Controlador
+ */
+app.controller('aeroController', ['$scope', 'aeroService', function ($scope, aeroService) {
+
+    //Objects
+    $scope.flights = [];
+    $scope.users = [];
+
+    function init()
+    {
+        //Users
+        aeroService.GetUsers()
+            .then(response => response.data)
+            .then(function (data) {
+                if (data != null) {
+                    $scope.users = data;
+                } 
+            });
+
+        if ($scope.users.length == 0) {
+            $scope.users = aeroService.users;
+        } 
+        
+        //Flights
+        aeroService.GetFlights()
+            .then(response => response.data)
+            .then(function (data) {
+                if (data != null) {
+                    $scope.flights = data;
+                    console.log($scope.flights);
+                }
+            });
+
+        if ($scope.flights.length == 0) {
+            $scope.flights = aeroService.flights;
+        } 
+    }
+
+    init();
+
+}]);
