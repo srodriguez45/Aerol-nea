2fa9df3ff01ad308a68e101891f7850c4369247e
Jair ®odriguez <jair3337@hotmail.com>
2019-11-25 22:20:16 -0500

    creación modulo de pruebas unitarias

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BLL.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var repo = new RFlights();
+            var result = repo.Get(2);
+
+            Assert.AreEqual("Yhon Cruz", result.pilotName);
+            Assert.AreEqual(2, result.idFlight);            
+        }
+    }
+}
